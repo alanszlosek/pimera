14b443ad6ad2ae6f33b169e4da1107d8f133358b
Alan Szlosek Jr <alan.szlosek@gmail.com>
2023-08-10 12:42:56 -0400

    mimic what telegraph cpu input does for cpu usage

--- cpu-metrics/main.c
+++ cpu-metrics/main.c
@@ -6,8 +6,6 @@
 #include <math.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <poll.h>
-#include <pthread.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -22,10 +20,12 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+// 10 seconds
+#define SLEEP_INTERVAL 10
+
 typedef long long int lli;
 
 bool running = true;
-pthread_mutex_t running_mutex;
 
 
 void logError(char const* msg, const char *func) {
@@ -63,14 +63,12 @@ void logInfo(char const* fmt, ...) {
 void signalHandler(int signal_number) {
     logError("Got signal. Exiting", __func__);
     // TODO: think there's an atomic variable meant to help with "signal caught" flags
-    pthread_mutex_lock(&running_mutex);
     running = false;
-    pthread_mutex_unlock(&running_mutex);
     logInfo("Signaled");
 }
 
 // TODO: clean up these variable names, error messages, etc
-void heartbeat() {
+void collect(char* statsd_server, char* statsd_port, bool collect_temperature) {
     bool r;
 
     int sockfd;
@@ -81,10 +79,11 @@ void heartbeat() {
     int cpuFd;
     char sTemperature[30];
     int iTemperature;
-    char metric[500];
+    char statsd_message[500];
     char hostname[100];
 
-    long ticks_per_second = sysconf(_SC_CLK_TCK);
+    // ticks per second x sleep seconds
+    long ticks_per_sleep = sysconf(_SC_CLK_TCK) * SLEEP_INTERVAL;
 
 
     if (gethostname(hostname, 100) != 0) {
@@ -97,7 +96,7 @@ void heartbeat() {
     hints.ai_socktype = SOCK_DGRAM;
 
     // Temperature metrics server info
-    if ((rv = getaddrinfo("192.168.1.173", "8125", &hints, &temperatureAddr)) != 0) {
+    if ((rv = getaddrinfo(statsd_server, statsd_port, &hints, &temperatureAddr)) != 0) {
         fprintf(stdout, "[ERR] Failed to getaddrinfo for temperature metrics server: %s\n", gai_strerror(rv));
         return;
     }
@@ -109,34 +108,32 @@ void heartbeat() {
 
 
     // Get initial CPU numbers
+    unsigned int total, total1, total2;
     unsigned int user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
     unsigned int user1, nice1, system1, idle1, iowait1, irq1, softirq1, steal1, guest1, guest_nice1;
     unsigned int user2, nice2, system2, idle2, iowait2, irq2, softirq2, steal2, guest2, guest_nice2;
     FILE *proc_stats = fopen("/proc/stat", "r");
     int bytes_read = fscanf(proc_stats, "cpu  %u %u %u %u %u %u %u %u %u %u", &user1, &nice1, &system1, &idle1, &iowait1, &irq1, &softirq1, &steal1, &guest1, &guest_nice1);
     fclose(proc_stats);
+    total1 = user1 + nice1 + system1 + iowait1 + irq1 + softirq1 + idle1;
 
-    pthread_mutex_lock(&running_mutex);
     r = running;
-    pthread_mutex_unlock(&running_mutex);
     while (r) {
+        sleep(SLEEP_INTERVAL);
 
-        cpuFd = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
-        numbytes = read(cpuFd, sTemperature, 30);
-        if (numbytes > 0) {
-            // We only need 10s and 1s digits
-            sTemperature[2] = 0;
-            iTemperature = atoi(sTemperature);
-            if (iTemperature >= 62) {
-                logError("CPU temperature is above 62", __func__);
+        if (collect_temperature) {
+            cpuFd = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
+            numbytes = read(cpuFd, sTemperature, 30);
+            if (numbytes > 0) {
+                // We only need 10s and 1s digits
+                sTemperature[2] = 0;
+                iTemperature = atoi(sTemperature);
+                if (iTemperature >= 62) {
+                    logError("CPU temperature is above 62", __func__);
+                }
             }
+            close(cpuFd);
         }
-        close(cpuFd);
-
-        iTemperature = 1;
-
-
-        // MORE WORK TO DO ON CPU USAGE BELOW
 
         // CPU Utilization
         proc_stats = fopen("/proc/stat", "r");
@@ -145,16 +142,14 @@ void heartbeat() {
 
         unsigned int cpu;
         if (bytes_read > 0) {
-            user = (user2 - user1) / ticks_per_second;
-            system = (system2 - system1) / ticks_per_second;
-            idle = (idle2 - idle1) / ticks_per_second;
+            total2 = user2 + nice2 + system2 + iowait2 + irq2 + softirq2 + idle2;
+            total = total2 - total1;
 
-            cpu = (
-                (float)((user2 - user1) + (system2 - system1))
-                /
-                (float)((user2 - user1) + (system2 - system1) + (idle2 - idle1))
-            ) * 100;
-            printf("cpu: %d\n", cpu);
+            user = 100 * ((user2 - user1) / (float)total);
+            system = 100 * ((system2 - system1) / (float)total);
+            idle = 100 * ((idle2 - idle1) / (float)total);
+
+            //printf("usage: %d\n", 100 - idle);
 
             user1 = user2;
             nice1 = nice2;
@@ -166,21 +161,40 @@ void heartbeat() {
             steal1 = steal2;
             guest1 = guest2;
             guest_nice1 = guest_nice2;
+            total1 = total2;
         }
 
+        if (collect_temperature) {
+            numbytes = snprintf(
+                statsd_message,
+                499,
+                "raspi.celsius.cpu,host=%s:%d|g\ncpu.usage_user,host=%s:%d|g\ncpu.usage_system,host=%s:%d|g\ncpu.usage_idle,host=%s:%d|g",
+                hostname, iTemperature,
+                
+                hostname, user,
+                
+                hostname, system,
+                
+                hostname, idle
+            );
+        } else {
+            numbytes = snprintf(
+                statsd_message,
+                499,
+                "cpu.usage_user,host=%s:%d|g\ncpu.usage_system,host=%s:%d|g\ncpu.usage_idle,host=%s:%d|g",
+                hostname, user,
+                
+                hostname, system,
+                
+                hostname, idle
+            );
+        }
 
-        numbytes = snprintf(metric, 200, "raspi.celsius.cpu,host=%s:%d|g\ncpu.usage_idle,host=%s2:%d|g", hostname, iTemperature, hostname, idle );
-
-        //printf("Metric: %s\n", metric);
-        if ((numbytes = sendto(sockfd, metric, numbytes, 0, temperatureAddr->ai_addr, temperatureAddr->ai_addrlen)) == -1) {
+        if ((numbytes = sendto(sockfd, statsd_message, numbytes, 0, temperatureAddr->ai_addr, temperatureAddr->ai_addrlen)) == -1) {
             fprintf(stderr, "Failed to send udp temperature metric. errno: %d\n", errno);
         }
 
-        sleep(10);
-
-        pthread_mutex_lock(&running_mutex);
         r = running;
-        pthread_mutex_unlock(&running_mutex);
     }
     close(sockfd);
     freeaddrinfo(temperatureAddr);
@@ -190,10 +204,26 @@ void heartbeat() {
 
 
 int main(int argc, const char **argv) {
-    pthread_mutex_init(&running_mutex, NULL);
-
     signal(SIGINT, signalHandler);
+    char* statsd_server = getenv("STATSD_SERVER");
+    char* statsd_port = getenv("STATSD_PORT");
+    char* skip_temperature = getenv("SKIP_TEMPERATURE");
+    bool collect_temperature = true;
 
-    // heartbeat loop
-    heartbeat();
+    if (!statsd_server) {
+        printf("Please specify STATSD_SERVER environment variable. Exiting.\n");
+        return 1;
+    }
+    if (!statsd_port) {
+        statsd_port = "8125";
+    }
+    if (!skip_temperature) {
+        collect_temperature = false;
+    }
+
+    
+
+    // TODO: pull server and port from envvars
+
+    collect(statsd_server, statsd_port, collect_temperature);
 }
