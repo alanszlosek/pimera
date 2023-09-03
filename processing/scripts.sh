ff7ccabfb9dc3223607763da7bbabb59c14bb82b
Alan Szlosek Jr <alan.szlosek@gmail.com>
2023-09-03 17:40:01 -0400

    processing should stop if child script errors

--- processing/scripts.sh
+++ processing/scripts.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # BE SURE TO UPDATE ../config.json WITH THE CORRECT VALUES FOR YOUR SETUP
+# AND THE PATHS BELOW
 
 # RUN ALL PROCESSING SCRIPTS
 
 # change into this directory
 cd -- "$(dirname "$0")"
 
-if [[ ! -d "./venv"]]; then
+if [[ ! -d "./venv" ]]; then
     python3 -m venv ./venv
+    source venv/bin/activate
     pip install mysql-connector-python ultralytics Pillow opencv-python
+else
+    source venv/bin/activate
 fi
-source venv/bin/activate
 
-./01.copy.sh
-python3 ./02.convert.py /mnt/media/surveillance
-python3 ./03.index.py
-python3 ./04.detection.py
+./01.copy.sh /mnt/media/surveillance/h264/ || exit 1
+python3 ./02.convert.py /mnt/media/surveillance || exit 1
+python3 ./03.index.py || exit 1
+python3 ./04.detection.py || exit 1
 
